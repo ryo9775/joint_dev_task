3434641d57eabbfa636d15ade150710b8468fe52
ryoimahata <ryoimahata@gmail.com>
2021-03-14 18:30:41 +0900

    q2の修正、q5~8の回答を追加

--- task.rb
+++ task.rb
@@ -14,8 +14,7 @@ def q2
   array2 = ["bird", "bat", "tiger"]
 
   # 以下に回答を記載
-  array1 << (array2)
-  array1.flatten!
+  array1 = array1 + array2
   p array1
 end
 
@@ -41,20 +40,22 @@ def q5
   array2 = [1, 5, 8, 10]
 
   # 以下に回答を記載
-
+  p array1.empty?
+  p array2.empty?
 end
 
 def q6
   numbers1 = [1, 2, 3, 4, 5]
 
   # 以下に回答を記載
-
+  p numbers1.map { |n| n * 10 }
 end
 
 def q7
   array = ["1", "2", "3", "4", "5"]
 
   # 以下に回答を記載
+  array.map!(&:to_i)
 
   # 以下は変更しないで下さい
   p array
@@ -64,6 +65,8 @@ def q8
   programming_languages = %w(ruby php python javascript)
 
   # 以下に回答を記載
+  programming_languages.map!(&:capitalize)
+  upper_case_programming_languages = programming_languages.map(&:upcase)
 
   # 以下は変更しないで下さい
   p programming_languages
@@ -74,7 +77,6 @@ def q9
   names = ["田中", "佐藤", "佐々木", "高橋"]
 
   # 以下に回答を記載
-
 end
 
 def q10
