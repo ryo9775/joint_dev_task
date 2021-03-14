f09ca1df67b2d006bcf54bbb7911bf85b1f1d456
ryoimahata <ryoimahata@gmail.com>
2021-03-15 01:45:44 +0900

    q13~16の回答を追加

--- task.rb
+++ task.rb
@@ -119,14 +119,16 @@ def q13
   update_data = { age: 32, address: "沖縄" }
 
   # 以下に回答を記載
-
+  user_data[:age] = 32
+  user_data[:address] = "沖縄"
+  p user_data
 end
 
 def q14
   data = { name: "satou", age: 33, address: "saitama", hobby: "soccer", email: "hoge@fuga.com" }
 
   # 以下に回答を記載
-
+  p data.keys
 end
 
 def q15
@@ -134,7 +136,16 @@ def q15
   data2 = { name: "yamada", hobby: "baseball", role: "normal" }
 
   # 以下に回答を記載
-
+  if data1[:age]
+    puts "OK"
+  else
+    puts "NG"
+  end
+  if data2[:age]
+    puts "OK"
+  else
+    puts "NG"
+  end
 end
 
 def q16
@@ -146,7 +157,9 @@ def q16
   ]
 
   # 以下に回答を記載
-
+    users.each do |user|
+      puts "私の名前は#{user[:name]}です。年齢は#{user[:age]}歳です"
+    end
 end
 
 class UserQ17
